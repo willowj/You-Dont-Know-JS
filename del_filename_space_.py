bbcdff3c4787a37b3a52773d6b2ead7ea2a23301
willowj <765755345@qq.com>
2018-05-23 23:49:44 +0800

    amend file path

--- del_filename_space_.py
+++ del_filename_space_.py
@@ -10,3 +10,12 @@ for z in os.listdir(os.curdir):
     if os.path.isdir(z) and not z.startswith('.'):
         os.rename(z,z.replace(' & ','_vs_'))
 
+    if z.lower() == 'readme.md':
+        with open(z,'a+') as f:
+            art = f.read()
+            print art
+            art = art.replace('\%20&\%20','_vs_')
+
+        with open(z,'w+') as f:
+            print art
+            f.write(art)
